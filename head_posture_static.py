99be44c6b8bc696dd60022b349d1f886a0651f02
Remy Mellet <remy.mellet@gmail.com>
2023-01-16 19:44:05 +0100

    makes head_posture_static iso to upstream

--- head_posture_static.py
+++ head_posture_static.py
@@ -20,7 +20,7 @@ drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=3)
 # points_idx.sort()
 
 # uncomment next line to use all points for PnP algorithm
-points_idx = list(range(0,468)); #points_idx[0:2] = points_idx[0:2:-1];
+points_idx = list(range(0,468)) #points_idx[0:2] = points_idx[0:2:-1];
 nose_idx = points_idx.index(4)
 
 # dir = "/home/remmel/workspace/dataset/vv/kinects/mediapipe/k1-left/"
@@ -30,7 +30,10 @@ nose_idx = points_idx.index(4)
 # f = "/media/remmel/DATASSDEXT/photogrammetry/dataset/2022-06-29_kinect/k2-landscape-horizontal/original/000022_rgb-mirror.jpg"
 # f = "/media/remmel/DATASSDEXT/photogrammetry/dataset/vv/kinects/mediapipe/k2-move/left/image.jpg"
 # f = "/media/remmel/PHOTOGRA/dataset/2021-12-08_kinectv2x2/seated/k21-180.jpg"
-f = "/media/remmel/DATASSDEXT/photogrammetry/dataset/vv/kinects/mediapipe/py-k2-mediapipeface/0266_rgb-mirror.jpg"
+# f = "/media/remmel/DATASSDEXT/photogrammetry/dataset/2022-06-29_kinect/k2-landscape-horizontal/original/000022_rgb-mirror.jpg"
+# f = "/media/remmel/DATASSDEXT/photogrammetry/dataset/vv/kinects/mediapipe/k2-move/left/image.jpg"
+# f = "/media/remmel/PHOTOGRA/dataset/2021-12-08_kinectv2x2/seated/k21-180.jpg"
+f = "/media/remmel/DATASSDEXT/photogrammetry/dataset/vv/kinects/mediapipe/py-k2-mediapipeface/1336_rgb-mirror.jpg"
 frame_height, frame_width, channels = (1080, 1920, 3)
 focal_length = 1081
 
@@ -46,6 +49,9 @@ dist_coeff = np.zeros((4, 1))
 
 
 def main():
+
+    refine_landmarks = True
+
     pcf = PCF(
         near=1, #1cm
         far=10000,
@@ -55,8 +61,8 @@ def main():
     )
 
     with mp_face_mesh.FaceMesh(
-        refine_landmarks=True,
         static_image_mode=True,
+        refine_landmarks=refine_landmarks,
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5,
     ) as face_mesh:
@@ -74,34 +80,44 @@ def main():
             # print(landmarks.shape)
             landmarks = landmarks.T
 
-            landmarks = landmarks[:, :468]
+            if refine_landmarks:
+                landmarks = landmarks[:, :468]
 
             metric_landmarks, pose_transform_mat = get_metric_landmarks(
                 landmarks.copy(), pcf
             )
-            model_points = metric_landmarks[0:3, points_idx].T
+
             image_points = (
                 landmarks[0:2, points_idx].T
                 * np.array([frame_width, frame_height])[None, :]
             )
 
-            success, rotation_vector, translation_vector = cv2.solvePnP(
-                model_points,
-                image_points,
-                camera_matrix,
-                dist_coeff,
-                flags=cv2.cv2.SOLVEPNP_ITERATIVE,
-            )
+            model_points = metric_landmarks[0:3, points_idx].T
 
-            exportToJson(face_landmarks, model_points, points_idx, rotation_vector, translation_vector, f)
+            # see here:
+            # https://github.com/google/mediapipe/issues/1379#issuecomment-752534379
+            pose_transform_mat[1:3, :] = -pose_transform_mat[1:3, :]
+            mp_rotation_vector, _ = cv2.Rodrigues(pose_transform_mat[:3, :3])
+            mp_translation_vector = pose_transform_mat[:3, 3, None]
 
-            (nose_end_point2D, jacobian) = cv2.projectPoints(
-                np.array([(0.0, 0.0, 25.0)]),
-                rotation_vector,
-                translation_vector,
-                camera_matrix,
-                dist_coeff,
-            )
+            if False:
+                # sanity check
+                # get same result with solvePnP
+
+                success, rotation_vector, translation_vector = cv2.solvePnP(
+                    model_points,
+                    image_points,
+                    camera_matrix,
+                    dist_coeff,
+                    flags=cv2.cv2.SOLVEPNP_ITERATIVE,
+                )
+
+                np.testing.assert_almost_equal(mp_rotation_vector, rotation_vector)
+                np.testing.assert_almost_equal(
+                    mp_translation_vector, translation_vector
+                )
+
+            exportToJson(face_landmarks, model_points, points_idx, mp_rotation_vector, mp_translation_vector, f)
 
             for face_landmarks in multi_face_landmarks:
                 mp_drawing.draw_landmarks(
@@ -112,10 +128,21 @@ def main():
                     connection_drawing_spec=drawing_spec,
                 )
 
-            p1 = (int(image_points[nose_idx][0]), int(image_points[nose_idx][1]))
-            p2 = (int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+            nose_tip = model_points[nose_idx]
+            nose_tip_extended = 2.5 * nose_tip
+            (nose_pointer2D, jacobian) = cv2.projectPoints(
+                np.array([nose_tip, nose_tip_extended]),
+                mp_rotation_vector,
+                mp_translation_vector,
+                camera_matrix,
+                dist_coeff,
+            )
 
-            frame = cv2.line(frame, p1, p2, (255, 0, 0), 2)
+            nose_tip_2D, nose_tip_2D_extended = nose_pointer2D.squeeze().astype(int)
+            frame = cv2.line(
+                frame, nose_tip_2D, nose_tip_2D_extended, (255, 0, 0), 2
+            )
+
             cv2.imwrite(f + ".drawlandmarks.jpg", frame)
 
             # source.show(frame)
